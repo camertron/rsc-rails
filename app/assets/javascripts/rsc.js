0a90cf16698fbf3c71a2c9f5b8a984685081b063
Cameron Dutro <camertron@gmail.com>
2018-08-26 18:10:57 -0700

    Update

--- app/assets/javascripts/rsc.js
+++ app/assets/javascripts/rsc.js
@@ -1,8 +1,35 @@
 //= require 'rsc/rsc.min.js'
 
 function init() {
-  $('.rsc-container').each(function() {
-    new Rsc($(this));
+  $('.rsc-rails').each(function() {
+    var program = $('.rsc-program', $(this));
+    var interpreter = $('.rsc-interpreter', $(this));
+
+    if (program.length == 0) {
+      program = $("<input class='rsc-program' type='hidden' />");
+      $(this).append(program);
+    }
+
+    if (interpreter.length == 0) {
+      interpreter = $("<div class='rsc-interpreter'></div>");
+      $(this).append(interpreter);
+    }
+
+    var options = {
+      loadProgram: function() {
+        try {
+          return JSON.parse(atob(program.val()));
+        } catch(e) {
+          return {};
+        }
+      },
+
+      saveProgram: function(commandList) {
+        program.val(commandList.toBase64());
+      }
+    }
+
+    new Rsc(interpreter, options);
   });
 };
 
